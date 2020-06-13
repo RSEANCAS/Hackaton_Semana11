17bd3f38bca71c88e6fd11fd40c2b9cd7d8622f3
RSEANCAS <ronald.jsb@hotmail.com>
2020-06-13 13:38:52 -0500

    ReMejoras Reto 1

--- Reto 1/js/main.js
+++ Reto 1/js/main.js
@@ -6,8 +6,22 @@ const calculadora = {
     init() {
         document.querySelectorAll('.calculadora .valores button').forEach(x => x.addEventListener('click', calculadora.events.valoresClick));
         document.querySelectorAll('.calculadora .operaciones button').forEach(x => x.addEventListener('click', calculadora.events.operacionesClick));
+        document.querySelector('.calculadora .resultado input').addEventListener("keydown", calculadora.events.inputKeyDown);
     },
     events: {
+        inputKeyDown(e) {
+            let listaOperaciones = Array.from(document.querySelectorAll('.calculadora .operaciones .operador')).map(x => { return { valor: x.getAttribute('value'), texto: x.getAttribute('text') }; });
+            let listaOperacionesValores = listaOperaciones.map(x => x.texto.length == 1 ? x.texto : `(${x.texto})`);
+
+            let regexText = listaOperacionesValores.map(x => x.length == 1 ? `\\${x}` : x).join('');
+            let regexValue = RegExp(`[${regexText}]+`);
+
+            console.log(regexValue);
+            let texto = String.fromCharCode(e.keyCode).toLowerCase();
+            if (texto.match(/[0-9.]/)) document.querySelector(`.valor[value="${texto}"]`).click();
+            if (texto.match(regexValue)) document.querySelector(`.operador[text="${texto}"]`).click();
+            console.log(texto);
+        },
         valoresClick(e) {
             let button = e.currentTarget;
             let valor = button.textContent;
